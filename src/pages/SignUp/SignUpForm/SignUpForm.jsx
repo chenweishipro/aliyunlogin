647e47f0a73d9d02d1ed765b4badabd26b457879
ChenWeishi <chen.wei.shi@icloud.com>
2024-09-23 22:22:30 +0800

    前端验证错误提示

--- src/pages/SignUp/SignUpForm/SignUpForm.jsx
+++ src/pages/SignUp/SignUpForm/SignUpForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './SignUpForm.less'
-import api from '../../../api';
+import api from '../../../api'
+import classnames from 'classnames'
 
 export default function SignUpForm() {
 
@@ -10,6 +11,7 @@ export default function SignUpForm() {
   let [email, setEmail] = useState('xxxx@3sbio.com');
   let [password, setPassword] = useState('');
   let [confirmPwd, setConfirmPwd] = useState('');
+  let [errors, setErrors] = useState({});
 
 function onSubmit(e) {
   // 提交表单，获取元素，发送后台
@@ -19,7 +21,26 @@ function onSubmit(e) {
     username,
     email,
     password,
-    confirmPwd}).then(res=>{console.log(res.data);})
+    confirmPwd}).then(res=>{console.log(res.data);
+      if (res.data.status===200) {
+        alert("注册成功！")
+      }
+
+      if(res.data.status===401) {
+        alert("注册失败！")
+      }
+
+      if(res.data.status===400) {
+        alert("输入信息不合法，验证失败！")
+        log(res.data.errors);
+        setErrors(res.data.errors)
+      }
+
+
+    }).catch(err=>{
+      console.log(err);
+      
+    })
 }
 
   return (
@@ -27,14 +48,20 @@ function onSubmit(e) {
       <h2 className='title'>欢迎注册阿里云</h2>
       <form onSubmit={onSubmit}>
       <div className="form-group">
-        <input type="text" value = {username} onChange={(e)=>{setUsername(e.target.value)}} className="form-control"  placeholder='请输入用户名'/>
+        <input type="text" value = {username} onChange={(e)=>{setUsername(e.target.value)}} 
+        
+        
+        className={classnames('form-control',{'is-invalid':errors.username})}
+       
+        
+        placeholder='请输入用户名'/>
         <small   className="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
 
       <div className="form-group">
         <input type="email" value={email} 
         onChange={(e)=>{setEmail(e.target.value)}}
-        className="form-control"  placeholder='请输入邮箱'/>
+        className={classnames('form-control', {'is-invalid':errors.email})}  placeholder='请输入邮箱'/>
        
       </div>
 
